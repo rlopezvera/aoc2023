2a5d10037fb29cd72f8b201dc883c40886987b69
Rodrigo <rlopezvera98@gmail.com>
2023-12-13 21:31:12 -0500

    day 4 part 2

--- python/day4/day4.py
+++ python/day4/day4.py
@@ -13,9 +13,13 @@ def main():
     with open(file_name, "r") as f:
         input = f.read().strip().split("\n")
 
+    ic(input)
     counter = defaultdict(int)
     p1 = 0
+    p2 = 0
+
     for i, line in enumerate(input):
+        counter[i] += 1
         first, rest = line.split("|")
         id_, card = first.split(":")
         card_nums = [int(x) for x in card.split()]
@@ -23,7 +27,14 @@ def main():
         val = len(set(card_nums) & set(user_cards))
         if val > 0:
             p1 += 2**(val-1)
+        for j in range(val):
+            counter[i+j+1] += counter[i]
+        # 0
+        # [0, 1, 2, 3]
+        # [1 += 0]
+
     ic(p1)
+    ic(p2)
 
     return
 
