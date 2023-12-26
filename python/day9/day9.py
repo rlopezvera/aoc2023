65ab8de87fcecf65f18f551bcf12f394b41fb012
Rodrigo <rlopezvera98@gmail.com>
2023-12-25 19:20:46 -0500

    day9 p2

--- python/day9/day9.py
+++ python/day9/day9.py
@@ -14,20 +14,29 @@ def main() -> None:
     file = open(file_name, "r").read().strip()
     input = [[int(y) for y in x.split()] for x in file.split("\n")]
     p1 = []
+    p2 = []
     for history in input:
         tmp = history
         steps = 0
         last_diff = [history[-1]]
+        first_diff = [history[0]]
         while not check_all_zero(tmp):
             steps += 1
             tmp = [tmp[i] - tmp[i-1]  # type: ignore
                    for i in range(1, len(tmp))]
             last_diff.append(tmp[-1])
+            first_diff.append(tmp[0])
 
-        ic(last_diff)
+        cum = 0
+        xd = list(reversed(first_diff))[1:]
+        for i in range(len(xd)):
+            cum = xd[i] - cum
         p1.append(sum(last_diff))
+        p2.append(cum)
+
         # break
     ic(sum(p1))
+    ic(sum(p2))
 
 
 if __name__ == "__main__":
